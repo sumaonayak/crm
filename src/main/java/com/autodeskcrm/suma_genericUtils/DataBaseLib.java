14452cfb869b9bb7c5224e7c79d758721fefeafe
User <User@Win7x64>
2020-08-04 17:28:29 +0530

    generic classes

---
+++ src/main/java/com/autodeskcrm/suma_genericUtils/DataBaseLib.java
@@ -0,0 +1,24 @@
+package com.autodeskcrm.suma_genericUtils;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class DataBaseLib {
+	
+	
+	public void connectToDB() {
+		//1 register Driver
+		//2.connect to db
+		//3. create statement
+	}
+	
+	public Resultset executeQuery(String query) {
+		return null;
+		//execute query & return the result
+	}
+	
+	
+	public void dissconnectToDb() {
+		//5 . close db
+	}
+
+}
